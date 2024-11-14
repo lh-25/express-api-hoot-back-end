81fd791e9be40b3b5bd5bd8ede4189a2883b0cb1
lh-25 <sprinklecake25@gmail.com>
2024-11-14 14:20:01 -0600

    hoot lab

---
+++ controllers/hoots.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const verifyToken = require('../middleware/verify-token')
+const Hoot = require('../models/hoot')
+const router = express.Router()
+
+// ========== Public Routes ===========
+
+// ========= Protected Routes =========
+router.use(verifyToken)
+// POST /hoots
+router.post('/', async (req, res) => {
+  try {
+    req.body.author = req.user.id
+    const hoot = await Hoot.create(req.body)
+    hoot._doc.author = req.user
+    res.status(201).json(hoot)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+})
+// GET /hoots
+router.get('/', async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate('author')
+      .sort({ createdAt: 'desc' });
+    res.status(200).json(hoots);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET /hoots/:hootId
+
+router.get('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId).populate('author')
+    res.status(200).json(hoot)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// PUT /hoots/:hootId
+
+router.put('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId)
+    // Check permissons
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!")
+    }
+    // Update hoot
+    const updatedHoot = await Hoot.findByIdAndUpdate(req.params.hootId, req.body, {new: true} )
+
+    // Append req.user to the author property
+    updatedHoot._doc.author = req.user
+
+    // Issue JSON response
+    res.status(200).json(updatedHoot)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// DELETE /hoots/:hootId
+
+router.delete('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId)
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!")
+    }
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId)
+    res.status(200).json(deletedHoot)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// POST /hoots/:hootId/comments
+
+router.post('/:hootId/comments', async (req, res) => {
+  try {
+    req.body.author = req.user._id
+    const hoot = await Hoot.fiindById(req.params.hootId)
+    hoot.comment.push(req.body)
+    await hoot.save()
+// Find the newly created comment 
+    const newComment = hoot.comments[hoot.comments.length - 1]
+    newComment._doc.author = req.user
+
+    //Respond with the newComment
+    res.status(201).json(newComment)
+
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+
+module.exports = router
---
+++ models/hoot.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  },
+  { timestamps: true }
+)
+
+const hootSchema = new mongoose.Schema(
+  {
+    title: { type: String, require: true },
+    text: { type: String, require: true },
+    category: { type: String, require: true, enum: ['News', 'Sports', 'Games', 'Movies', 'Music', 'Television'] },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    comments: [commentSchema]
+  },
+  { timestamps: true }
+)
+
+const Hoot = mongoose.model('Hoot', hootSchema)
+module.exports = Hoot
